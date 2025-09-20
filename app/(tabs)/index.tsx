5bc2d076210e380fb5d960cea96d515bd8e0cdf6
mabilisasandile <124358059+mabilisasandile@users.noreply.github.com>
2025-09-20 22:30:34 +0000

    Fetch/search movies logic

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,62 @@
-import { Image, ScrollView, Text, View } from "react-native";
+import { ActivityIndicator, FlatList, Image, ScrollView, Text, View } from "react-native";
 import { Link } from "expo-router";
 import { icons } from "@/constants/icons";
 import SearchBar from "@/components/SearchBar";
 import { useRouter } from "expo-router";
+import useFetch from "@/services/useFetch";
+import { fetchMovies } from "@/services/api";
+import MovieCard from "@/components/MovieCard";
+import { images } from "@/constants/images";
 
 export default function Index() {
 
   const router = useRouter();
 
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError
+  } = useFetch(() => fetchMovies({
+    query: ''
+  }));
+
   return (
     <View className="flex-1 bg-primary">
-      <Image source={icons.logo} className="absolute w-full z-0" />
+      <Image source={images.bg} className="absolute w-full z-0" />
 
-      <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{ minHeight: "100%", paddingBottom: 10}}>
+      <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}>
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-      
-        <View className="flex-1 mt-5">
-          <SearchBar  
-            onPress={() => router.push('/search')}
-            placeholder="Search for a movie"
-          />
-        </View>
+
+        {moviesLoading ? (
+          <ActivityIndicator size="large" color="#0000ff" className="mt-10 self-center" />
+        ) : moviesError ? (
+          <Text>Something went wrong: {moviesError?.message}</Text>
+        ) : (
+          <View className="flex-1 mt-5">
+            <SearchBar
+              onPress={() => router.push('/search')}
+              placeholder="Search for a movie"
+            />
+
+            <>
+              <Text className="text-lg text-white font-bold mt-5 mb-3">Latest Movies</Text>
+            
+              <FlatList
+                data={movies}
+                renderItem={({ item }) => (
+                  <MovieCard 
+                    {...item}
+                  />
+                )}
+                keyExtractor={(item) => item.id.toString()}
+                numColumns={3}
+                columnWrapperStyle={{ justifyContent: 'flex-start', marginBottom: 10, gap: 20, paddingRight: 5 }}
+                className="mb-2 pb-32"
+                scrollEnabled={false}
+              />
+            </>
+          </View>
+        )}
       </ScrollView>
     </View>
   );
--- services/api.ts
+++ services/api.ts
@@ -19,8 +19,8 @@ export const fetchMovies = async ({ query }: { query: string }) => {
     });
 
     if (!response.ok) {
-        // throw new Error('Failed to fetch movies', response.statusText);
-        throw new Error('Failed to fetch movies');
+        // @ts-ignore
+        throw new Error('Failed to fetch movies', response.statusText);
     }
 
     const data = await response.json();
